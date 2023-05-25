eb0e8869ad936c8d56888eda913d4ef2a2c63343
AlekseySamokhvalov <6560@mail.ru>
2023-05-26 01:09:21 +0300

    Добавили вывод по-русски.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome WORLD!!!!");
+        System.out.println("Hello and welcome WORLD!!!");
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 3; i++) {
 
@@ -12,5 +12,6 @@ public class Main {
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
+        System.out.println("Здравствуйте и добро пожаловать!!!");
     }
 }
